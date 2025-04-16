e59a7f973da70fa33b038aa5c818b5608f6270d4
AryanMulik <aryann.mulik@gmail.com>
2025-04-16 12:46:14 +0530

    Create jenkins_aryan.java

---
+++ jenkins_aryan.java
@@ -0,0 +1,20 @@
+public class jenkins_aryan {
+    
+    // Method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // Print Hello, World!
+        System.out.println("Hello, World!");
+
+        // Example of adding two numbers
+        int num1 = 5;
+        int num2 = 10;
+        int sum = add(num1, num2);
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
